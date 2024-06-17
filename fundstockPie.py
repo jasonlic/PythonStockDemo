2e06a6026438d524f882d8fff2087f4a82e2b2cc
jason_li_c <jason_li_c@hotmail.com>
2024-06-17 19:25:43 +0800

    right

--- fundstockPie.py
+++ fundstockPie.py
@@ -9,13 +9,14 @@ mpl.rcParams["font.sans-serif"] = ["SimHei"]
 mpl.rcParams["axes.unicode_minus"]=False
 
 df = pd.read_csv('fundstocks.cvs')
-#df.drop(df[(df.股数 == 'NaN')].index,inplace=True)
-print(df)
+
+#print(df)
 
 df['总价'] = df['最新价'] * df['股数']
 
-df_restore = df.sort_values(by='总价')
-df_20 = df_restore.loc[0:20]
+df.sort_values(by='总价',ascending=False,inplace=True)
+
+df_20 = df.iloc[0:20,:]
 print(df_20)
-#df_20['总价'].plot.pie(labels=df['名称'])
-#plt.show()
+df_20['总价'].plot.pie(labels=df['名称'],autopct='%.2f')
+plt.show()
